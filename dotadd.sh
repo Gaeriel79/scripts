3c438f8eeba915cad54db8c2ab65b7f7fbef51c7
Gaeriel79 <karstenf79@gmail.com>
2022-02-01 10:39:21 +0100

    3

---
+++ dotadd.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.xinitrc
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.bashrc_aliases
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.bashrc
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.xmonad/xmonad.hs
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.config/kitty/kitty.conf
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.config/picom/picom.conf
