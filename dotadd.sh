e07c45be6c96283ac90e7de95d135169a8c00fb8
Gaeriel79 <92832022+Gaeriel79@users.noreply.github.com>
2023-10-25 10:39:12 +0200

    Add SSH Keygen Script

--- dotadd.sh
+++
@@ -1,9 +0,0 @@
-
-#-------------------------------------------------------------------------
-/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.xinitrc
-/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.bashrc_aliases
-/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.bashrc
-/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.xmonad/xmonad.hs
-/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.config/kitty/kitty.conf
-/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add ~/.config/picom/picom.conf
-echo "dot updated"
