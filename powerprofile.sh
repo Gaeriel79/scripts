598cf0250ec910646a6b9c0917b57c30cc694eb7
Gaeriel79 <92832022+Gaeriel79@users.noreply.github.com>
2025-05-29 12:26:21 +0200

    Update powerprofile.sh

--- powerprofile.sh
+++ powerprofile.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Check for logged-in users
+logged_in_users=$(who | wc -l)
+
+# If there are no logged-in users, execute the power management command
+if [ "$logged_in_users" -eq 0 ]; then
+    echo "No users are logged in. Executing power management command."
+    power-adm profile powersave
+else
+    echo "$logged_in_users user(s) are currently logged in. Power management command will not be executed."
+fi
